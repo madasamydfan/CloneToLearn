5502b6e233ae1a432a535b229cbb369bf41f6525
sujith <sujith43465@gmail.com>
2025-07-30 11:08:05 +0530

    Home page in the making

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import './Ho'
+export default function HomePage() {
+  return (
+    <>
+      <div className="HomePage-container">
+        <div className="Header-container">
+          <div className="logo-netflix">NETFLIX</div>
+          <div className="two-button-container">
+            <button className="Language-button">ENGLISH</button>
+            <button className="Signin-button">SIGN IN</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
